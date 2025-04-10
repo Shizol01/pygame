79e448c4163943989cf105fec5da8c9b181fa20f
Shizol01 <wrzosek-krystian@wp.pl>
2025-04-10 12:10:46 +0200

    Created gitignore/requirements, written basics of main. Main opens a black screen and closes

---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame
+from constants import (SCREEN_HEIGHT, SCREEN_WIDTH,
+                        ASTEROID_KINDS, ASTEROID_MAX_RADIUS,
+                          ASTEROID_MIN_RADIUS, ASTEROID_SPAWN_RATE)
+
+
+def main():
+    pygame.init
+    print("Starting Asteroids! \nScreen width: 1280 \nScreen height: 720")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        pygame.Surface.fill(screen,'black')
+        pygame.display.flip()
+
+
+
+if __name__ == "__main__":
+    main()
